9375558bbd1ef9ff1a9c045256d15383ce821349
vaibhav95685 <vaibhav95685@gmail.com>
2023-02-03 15:36:12 +0530

    ISL 4 and 5 completed

---
+++ apps/web/src/pages/Radio.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+
+function ExampleForm() {
+  const [selectedValue, setSelectedValue] = useState('Enable');
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <form>
+      <div style={{ display: 'inline-block',marginTop:"15px" }}>
+        <input
+          type="radio"
+          name="example"
+          value="Enable"
+          checked={selectedValue === 'value1'}
+          onChange={handleChange}
+          id="Enable"
+          style={{
+            display: 'none',
+          }}
+        />
+        <label
+          htmlFor="Enable"
+          style={{
+            display: 'inline-block',
+            width: '20px',
+            height: '20px',
+            background: '#fff',
+            border: '2px solid #f8567f',
+            borderRadius: '50%',
+            cursor: 'pointer',
+            padding:"10px",
+            position: 'relative',
+          }}
+        >
+          {selectedValue === 'Enable' && (
+            <span
+              style={{
+                position: 'absolute',
+                top: '4px',
+                left: '4px',
+                width: '12px',
+                height: '12px',
+                borderRadius: '50%',
+                
+                background: '#f8567f',
+              }}
+            />
+          )}
+        </label>
+        <span style={{
+            
+            
+            padding:"10px",
+           
+          }}>
+        Enable
+        </span>
+      </div>
+      <div style={{ display: 'inline-block' ,marginTop:"15px",marginLeft:"20px" }}>
+        <input
+          type="radio"
+          name="example"
+          value="Disable"
+          checked={selectedValue === 'Disable'}
+          onChange={handleChange}
+          id="Disable"
+          style={{
+            display: 'none',
+          }}
+        />
+        <label
+          htmlFor="Disable"
+          style={{
+            display: 'inline-block',
+            width: '20px',
+            height: '20px',
+            background: '#fff',
+            border: '2px solid #f8567f',
+            borderRadius: '50%',
+            cursor: 'pointer',
+            padding:"10px",
+            position: 'relative',
+          }}
+        >
+          {selectedValue === 'Disable' && (
+            <span
+              style={{
+                position: 'absolute',
+                top: '4px',
+                left: '4px',
+                width: '12px',
+                height: '12px',
+                borderRadius: '50%',
+                background: '#f8567f',
+              }}
+            />
+          )}
+        </label>
+        <span style={{
+            
+            
+            padding:"10px",
+           
+          }}>
+        Disable
+        </span>
+      </div>
+      
+    </form>
+  );
+}
+
+export default ExampleForm;
--- apps/web/src/pages/createlaunchpad.tsx
+++ apps/web/src/pages/createlaunchpad.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
+import Radio from './Radio';
+import Dropdown from './Dropdown';
+import Dropdown1 from './Dropdown1';
+
+
 const MultiStepForm = () => {
   const [step, setStep] = useState(1);
 
@@ -23,7 +28,7 @@ const MultiStepForm = () => {
    
     </Head>
       <div className="steps">
-        Step {step} of 3
+        Step {step} of 4
       </div>
       {step === 1 && (
         <Step1 handleNextStep={handleNextStep} />
@@ -44,7 +49,8 @@ const MultiStepForm = () => {
 const Step1 = ({ handleNextStep }) => (
 
   <div>
-    <h2>Step 1</h2>
+    <h2 style={{fontSize:"20px",marginTop:"10px", marginBottom:"5px"}}>Step 1 - Verify Token</h2>
+    <h3 style={{ color:"grey", marginBottom:"10px"}}>Enter the token address and verify</h3>
     <div   className="row setup-content" id="step-1">
         <div   className="col-xs-12">
             <div   className="col-md-12">
@@ -60,56 +66,212 @@ const Step1 = ({ handleNextStep }) => (
             </div>
         </div>
     </div>
-    <button type="button" onClick={handleNextStep}>Next</button>
+    <button  onClick={handleNextStep} style={{ backgroundColor: '#f8567f', color: '#fff', padding: '0.5em 1em',  borderRadius: '0.25em',border: 'none' }} className="nextBtn btn-lg pull-right" > Next </button>
+   
   </div>
 );
 
 const Step2 = ({ handleNextStep, handlePrevStep }) => (
   <div>
-    <h2>Step 2</h2>
+    <h2 style={{fontSize:"20px",marginTop:"10px", marginBottom:"5px"}}>Step 2 - DeFi Launchpad Info</h2>
+    <h3 style={{ color:"grey", marginBottom:"10px"}}>Enter the launchpad information that you want to raise , that should be enter all details about your presale</h3>
     <div   className="row setup-content" id="step-2">
         <div   className="col-xs-12">
             <div   className="col-md-12">
                 <div   className="form-group">
-                    <label className="control-label">Company Name</label>
-                    <input  type="text"     className="form-control" placeholder="Enter Company Name" />
+                    <label style={{ marginBottom:"15px"}} className="control-label">Presale rate</label>
+                    <input  type="text" className="form-control" placeholder="Enter Presale rate" />
                 </div>
                 <div   className="form-group">
-                    <label   className="control-label">Company Address</label>
-                    <input type="text"     className="form-control" placeholder="Enter Company Address"  />
+                    <label   className="control-label">Whitelist</label>
+                    <div>
+                    
+    <Radio/>
+
+<div style={{display:"flex", flexDirection:"row"}} >
+               <div style={{ marginBottom:"15px", marginTop:"15px"}}  className="form-group">
+                    <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Softcap (BNB)</label>
+                    <input  type="text" className="form-control" placeholder="Enter Softcap (BNB)" />
+                </div>
+
+                <div style={{ marginBottom:"15px",marginTop:"15px",}}   className="form-group">
+                    <label style={{ marginLeft:"55px",marginBottom:"15px",marginTop:"15px",}}  className="control-label">Hardcap (BNB)</label>
+                    <input  style={{ marginLeft:"55px"}}  type="text" className="form-control" placeholder="Enter Hardcap (BNB)" />
+                </div>
+
+ </div>
+
+ <div style={{display:"flex", flexDirection:"row"}} >
+               <div style={{ marginBottom:"15px", marginTop:"5px"}}  className="form-group">
+                    <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Minimum Buy (BNB)</label>
+                    <input  type="text" className="form-control" placeholder="Minimum Buy (BNB)" />
+                </div>
+
+                <div style={{ marginBottom:"15px",marginTop:"5px",}}   className="form-group">
+                    <label style={{ marginLeft:"55px",marginBottom:"15px",marginTop:"15px",}}  className="control-label">Maximum Buy (BNB)</label>
+                    <input  style={{ marginLeft:"55px"}}  type="text" className="form-control" placeholder="Maximum Buy (BNB)" />
+                </div>
+
+ </div>
+
+
+
+ <div   className="form-group">
+                    <label style={{ marginBottom:"15px"}} className="control-label">Refund Type</label>
+                    <div style={{display:"flex", flexDirection:"row"}}>
+  <div>
+ <Dropdown/>
+ </div>
+ <div style={{marginLeft:"55px"}}>
+ <Dropdown1/>
+ </div>
+                </div>
+
+ </div>
+ <div style={{display:"flex", flexDirection:"row"}} >
+               <div style={{ marginBottom:"15px", marginTop:"5px"}}  className="form-group">
+                    <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Liquidity %</label>
+                    <input  type="text" className="form-control" placeholder="Liquidity %" />
+                </div>
+
+                <div style={{ marginBottom:"15px",marginTop:"5px",}}   className="form-group">
+                    <label style={{ marginLeft:"55px",marginBottom:"15px",marginTop:"15px",}}  className="control-label">Listing rate</label>
+                    <input  style={{ marginLeft:"55px"}}  type="text" className="form-control" placeholder="Listing rate" />
+                </div>
+
+ </div>
+ <label style={{marginTop:"15px",}}  className="control-label">Select start time & end time (UTC)</label>
+ <div style={{display:"flex", flexDirection:"row"}} >
+  
+               <div style={{ marginBottom:"15px", marginTop:"5px"}}  className="form-group">
+              
+                    <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Start time (UTC)</label>
+                    <input  type="text" className="form-control" placeholder="Start time (UTC)" />
+                </div>
+
+                <div style={{ marginBottom:"15px",marginTop:"5px",}}   className="form-group">
+                    <label style={{ marginLeft:"55px",marginBottom:"15px",marginTop:"15px",}}  className="control-label">End time (UTC)</label>
+                    <input  style={{ marginLeft:"55px"}}  type="text" className="form-control" placeholder="End time (UTC)" />
+                </div>
+
+ </div>
+
+ <div style={{ marginBottom:"15px", marginTop:"5px"}}  className="form-group">
+                    <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Liquidity lockup (minutes)</label>
+                    <input  type="text" className="form-control" placeholder="Liquidity lockup (minutes)" />
+                </div>
+
+
+  </div>
                 </div>
             </div>
         </div>
     </div>
-    <button type="button" onClick={handlePrevStep}>Prev</button>
-    <button type="button" onClick={handleNextStep}>Next</button>
+    
+    <button onClick={handlePrevStep} style={{ backgroundColor: '#ccc', color: '#000', padding: '0.5em 1em',  borderRadius: '0.25em',border: 'none' }} className="prevBtn btn-lg pull-left" > Back </button>
+    <button onClick={handleNextStep} style={{ backgroundColor: '#f8567f', color: '#fff', padding: '0.5em 1em',  borderRadius: '0.25em',border: 'none' }} className="nextBtn btn-lg pull-right" > Next </button>
+
   </div>
 );
 const Step3 = ({ handleNextStep, handlePrevStep }) => (
   <div>
-    <h2>Step 3</h2>
+    <h2 style={{fontSize:"20px",marginTop:"10px", marginBottom:"5px"}}>Step 3 - Add Additional Info</h2>
+    <h3 style={{ color:"grey", marginBottom:"10px"}}>Let people know who you are</h3>
     <div   className="row setup-content" id="step-3">
         <div   className="col-xs-12">
             <div   className="col-md-12">
-                <div   className="form-group">
-                    <label   className="control-label">Company Name</label>
-                    <input  type="text"     className="form-control" placeholder="Enter Company Name" />
-                </div>
-                <div   className="form-group">
-                    <label   className="control-label">Company Address</label>
-                    <input  type="text"     className="form-control" placeholder="Enter Company Address"  />
+            <div style={{display:"flex", flexDirection:"row"}} >
+  
+  <div style={{ marginBottom:"15px", marginTop:"5px"}}  className="form-group">
+ 
+       <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Logo URL*</label>
+       <input  type="text" className="form-control" placeholder="Logo URL*" />
+   </div>
+
+   <div style={{ marginBottom:"15px",marginTop:"5px",}}   className="form-group">
+       <label style={{ marginLeft:"55px",marginBottom:"15px",marginTop:"15px",}}  className="control-label">Website</label>
+       <input  style={{ marginLeft:"55px"}}  type="text" className="form-control" placeholder="Website" />
+   </div>
+
+</div>
+
+<div style={{display:"flex", flexDirection:"row"}} >
+  
+  <div style={{ marginBottom:"15px", marginTop:"5px"}}  className="form-group">
+ 
+       <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Facebook</label>
+       <input  type="text" className="form-control" placeholder="Facebook*" />
+   </div>
+
+   <div style={{ marginBottom:"15px",marginTop:"5px",}}   className="form-group">
+       <label style={{ marginLeft:"55px",marginBottom:"15px",marginTop:"15px",}}  className="control-label">Twitter</label>
+       <input  style={{ marginLeft:"55px"}}  type="text" className="form-control" placeholder="Twitter" />
+   </div>
+
+</div>
+
+<div style={{display:"flex", flexDirection:"row"}} >
+  
+  <div style={{ marginBottom:"15px", marginTop:"5px"}}  className="form-group">
+ 
+       <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Github</label>
+       <input  type="text" className="form-control" placeholder="Github" />
+   </div>
+
+   <div style={{ marginBottom:"15px",marginTop:"5px",}}   className="form-group">
+       <label style={{ marginLeft:"55px",marginBottom:"15px",marginTop:"15px",}}  className="control-label">Telegram</label>
+       <input  style={{ marginLeft:"55px"}}  type="text" className="form-control" placeholder="Telegram" />
+   </div>
+
+</div>
+
+
+<div style={{display:"flex", flexDirection:"row"}} >
+  
+  <div style={{ marginBottom:"15px", marginTop:"5px"}}  className="form-group">
+ 
+       <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Instagram</label>
+       <input  type="text" className="form-control" placeholder="Instagram" />
+   </div>
+
+   <div style={{ marginBottom:"15px",marginTop:"5px",}}   className="form-group">
+       <label style={{ marginLeft:"55px",marginBottom:"15px",marginTop:"15px",}}  className="control-label">Discord</label>
+       <input  style={{ marginLeft:"55px"}}  type="text" className="form-control" placeholder="Discord" />
+   </div>
+
+</div>
+
+
+<div style={{display:"flex", flexDirection:"row"}} >
+  
+  <div style={{ marginBottom:"15px", marginTop:"5px"}}  className="form-group">
+ 
+       <label style={{ marginBottom:"15px",marginTop:"15px",}}  className="control-label">Reddit</label>
+       <input  type="text" className="form-control" placeholder="Reddit" />
+   </div>
+
+   <div style={{ marginBottom:"15px",marginTop:"5px",}}   className="form-group">
+       <label style={{ marginLeft:"55px",marginBottom:"15px",marginTop:"15px",}}  className="control-label">Youtube Video</label>
+       <input  style={{ marginLeft:"55px"}}  type="text" className="form-control" placeholder="Youtube Video" />
+   </div>
+
+</div>
+<div   className="form-group">
+                    <label style={{ marginBottom:"15px"}} className="control-label">Description</label>
+                    <input style={{ height:"150px"}} type="text" className="form-control" />
                 </div>
             </div>
         </div>
     </div>
-    <button type="button" onClick={handlePrevStep}>Prev</button>
-    <button type="button" onClick={handleNextStep}>Next</button>
+    <button onClick={handlePrevStep} style={{ backgroundColor: '#ccc', color: '#000', padding: '0.5em 1em',  borderRadius: '0.25em',border: 'none' }} className="prevBtn btn-lg pull-left" > Back </button>
+    <button onClick={handleNextStep} style={{ backgroundColor: '#f8567f', color: '#fff', padding: '0.5em 1em',  borderRadius: '0.25em',border: 'none' }} className="nextBtn btn-lg pull-right" > Next </button>
   </div>
 );
 
 const Step4 = ({ handlePrevStep }) => (
   <div>
-    <h2>Step 4</h2>
+    <h2 style={{fontSize:"20px",marginTop:"10px", marginBottom:"5px"}}>Step 4 - Finish</h2>
+    <h3 style={{ color:"grey", marginBottom:"10px"}}>Review your information</h3>
     <div   className="row setup-content" id="step-4">
         <div   className="col-xs-12">
             <div   className="col-md-12">
@@ -125,8 +287,9 @@ const Step4 = ({ handlePrevStep }) => (
             </div>
         </div>
     </div>
-    <button type="button" onClick={handlePrevStep}>Prev</button>
-    <button type="submit">Submit</button>
+    
+    <button onClick={handlePrevStep} style={{ backgroundColor: '#ccc', color: '#000', padding: '0.5em 1em',  borderRadius: '0.25em',border: 'none' }} className="prevBtn btn-lg pull-left" > Back </button>
+    <button style={{ backgroundColor: '#f8567f', color: '#fff', padding: '0.5em 1em',  borderRadius: '0.25em',border: 'none' }} className="prevBtn btn-lg pull-right" > Submit </button>
   </div>
 );
 
