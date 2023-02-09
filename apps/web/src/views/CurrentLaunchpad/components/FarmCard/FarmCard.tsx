1633e4036da703a653020a67b12f81f1bca450a0
Ritik <ritiktyagi01356@gmail.com>
2023-02-09 18:14:05 +0530

    commit

--- apps/web/src/views/CurrentLaunchpad/components/FarmCard/FarmCard.tsx
+++ apps/web/src/views/CurrentLaunchpad/components/FarmCard/FarmCard.tsx
@@ -172,7 +172,7 @@ const FarmCard: React.FC<React.PropsWithChildren<FarmCardProps>> = ({
   <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
 </svg>
 
-<Link href={"/detail"}>      
+<Link href={"/details2"}>      
    <button type="button" style={{
       backgroundColor: '#f8567f',
       color: 'white',
