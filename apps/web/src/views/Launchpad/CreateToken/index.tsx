a6a83ae0ed58c8af73174094c84e8e8e3e635101
Ritik <ritiktyagi01356@gmail.com>
2023-02-15 10:48:24 +0530

    commit

--- apps/web/src/views/Launchpad/CreateToken/index.tsx
+++ apps/web/src/views/Launchpad/CreateToken/index.tsx
@@ -1,148 +1,138 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
+import React, { useState } from 'react'
 import { AppBody } from 'components/App'
-import {
-  AtomBox
-
-} from '@pancakeswap/ui';
+import { AtomBox } from '@pancakeswap/ui'
 import { useMemo } from 'react'
-import {
-  Text,
-  Farm as FarmUI,
-  SearchInput,
-  Select,
-  PageHeader,
-  Heading
-
-} from '@pancakeswap/uikit'
+import { Text, Farm as FarmUI, SearchInput, Select, PageHeader, Heading } from '@pancakeswap/uikit'
 import styled from 'styled-components'
-import { useRouter } from 'next/router';
-import Divider from 'views/Farms/components/Divider';
-
-
-
+import { useRouter } from 'next/router'
+import Divider from 'views/Farms/components/Divider'
 
 const LabelWrapper = styled.div`
   > ${Text} {
     font-size: 12px;
   }
 `
+const Container = styled.div`
+   {
+  }
+`
 
+const MainContainer = styled.div`
+   {
+    margin-top: 50px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 85%;
+  }
+`
 
+const InputDiv = styled.div`
+   {
+    margin-bottom: 15px;
+    margin-top: 15px;
+  }
+`
+const ButtonDiv = styled.div`
+   {
+    margin-top: 30px;
+    padding-bottom: 100px;
+  }
+`
 
 const CreateToken = () => {
-
-
   const { pathname, query: urlQuery } = useRouter()
   const [_query, setQuery] = useState('')
   const normalizedUrlSearch = useMemo(() => (typeof urlQuery?.search === 'string' ? urlQuery.search : ''), [urlQuery])
   const query = normalizedUrlSearch && !_query ? normalizedUrlSearch : _query
-  const [showTooltip, setShowTooltip] = useState(false);
+  const [showTooltip, setShowTooltip] = useState(false)
   const handleChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value)
   }
   return (
     <PageHeader>
       <AppBody>
-        <div>
-          <Head>
-
-          </Head>
-
-          <div className="maincontainer"  >
-            <div>
-
-
-              <div>
+       
+          <MainContainer>
+            <Container>
+              <Container>
                 <AtomBox width="full" alignItems="center" flexDirection="column" padding="24px" borderBottom="1">
-                  <Heading marginBottom={"5px"} textAlign={"center"} as="h2"> Create Token</Heading>
-                  <Text textAlign={"center"} color="textSubtle" fontSize="17px">
+                  <Heading marginBottom={'5px'} textAlign={'center'} as="h2">
+                    {' '}
+                    Create Token
+                  </Heading>
+                  <Text textAlign={'center'} color="textSubtle" fontSize="17px">
                     You can create the token from here !
                   </Text>
                 </AtomBox>
 
-                <LabelWrapper style={{ marginTop: "20px" }}>
+                <LabelWrapper style={{ marginTop: '20px' }}>
                   <Select
                     options={[
                       {
-                        label: ('Standard Token'),
+                        label: 'Standard Token',
                         value: 'Standard Token',
                       },
                       {
-                        label: ('Liquidity Generator Token'),
+                        label: 'Liquidity Generator Token',
                         value: 'Liquidity Generator Token',
                       },
                       {
-                        label: ('Baby Token'),
+                        label: 'Baby Token',
                         value: 'Baby Token',
                       },
                       {
-                        label: ('Buyback Baby Token'),
+                        label: 'Buyback Baby Token',
                         value: 'Buyback Baby Token',
                       },
-
                     ]}
-
                   />
                 </LabelWrapper>
-
-
-              </div>
-              <div className="inputdiv"  >
-                <Text marginBottom={"10px"} color="textSubtle" fontSize="17px" textAlign="left">
+              </Container>
+              <InputDiv>
+                <Text marginBottom={'10px'} color="textSubtle" fontSize="17px" textAlign="left">
                   Name
                 </Text>
                 <LabelWrapper>
                   <SearchInput onChange={handleChangeQuery} placeholder="Name" />
-
-
                 </LabelWrapper>
-              </div>
-              <div className="inputdiv"   >
-                <Text marginBottom={"10px"} color="textSubtle" fontSize="17px" textAlign="left">
+              </InputDiv>
+              <InputDiv>
+                <Text marginBottom={'10px'} color="textSubtle" fontSize="17px" textAlign="left">
                   Symbol
                 </Text>
                 <LabelWrapper>
                   <SearchInput onChange={handleChangeQuery} placeholder="Symbol" />
                 </LabelWrapper>
-              </div>
-              <div className="inputdiv"   >
-                <Text marginBottom={"10px"} color="textSubtle" fontSize="17px" textAlign="left">
+              </InputDiv>
+              <InputDiv>
+                <Text marginBottom={'10px'} color="textSubtle" fontSize="17px" textAlign="left">
                   Decimals
                 </Text>
 
                 <LabelWrapper>
                   <SearchInput onChange={handleChangeQuery} placeholder="Decimals" />
                 </LabelWrapper>
-              </div>
-              <div className="inputdiv"  >
-                <Text marginBottom={"10px"} color="textSubtle" fontSize="17px" textAlign="left">
+              </InputDiv>
+              <InputDiv>
+                <Text marginBottom={'10px'} color="textSubtle" fontSize="17px" textAlign="left">
                   Total Supply
                 </Text>
-
 
                 <LabelWrapper>
                   <SearchInput onChange={handleChangeQuery} placeholder="Total Supply" />
                 </LabelWrapper>
-
-
-              </div>
-
-            </div>
+              </InputDiv>
+            </Container>
 
             <Divider margin="0px" />
-            <div className="buttondiv">
-              <button className="nextBtn btn-lg pull-left bottombutton" >Create Token</button>
-            </div>
-
-
-          </div>
-        </div>
+            <ButtonDiv>
+              <button className="nextBtn btn-lg pull-left bottombutton">Create Token</button>
+            </ButtonDiv>
+          </MainContainer>
+      
       </AppBody>
     </PageHeader>
-  );
-};
+  )
+}
 
-
-
-export default CreateToken;
+export default CreateToken
---
+++ apps/web/src/views/Launchpad/CurrentLaunchpad/components/Card.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react'
+import { useMemo } from 'react'
+import { Text, Flex, Card } from '@pancakeswap/uikit'
+import styled from 'styled-components'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const StyledCard = styled(Card)`
+  align-self: baseline;
+  max-width: 100%;
+  margin: 0 0 24px 0;
+  ${({ theme }) => theme.mediaQueries.sm} {
+    max-width: 350px;
+    margin: 0 12px 46px;
+  }
+`
+
+const CurrentLaunchpadCardInnerContainer = styled(Flex)`
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 24px;
+  width: fixed;
+`
+const ProgresBar = styled.div`
+   {
+    width: 70%;
+    height: 100%;
+    background-color: #007bff;
+    border-radius: 10px;
+    transition: width 0.2s ease-in-out;
+  }
+`
+const LaunchpadLogo = styled.div`
+   {
+    width: 40px;
+    height: 40px;
+   
+  }
+`
+
+const CurrentLaunchpadCard = () => {
+  const { pathname, query: urlQuery } = useRouter()
+  const [_query, setQuery] = useState('')
+  const normalizedUrlSearch = useMemo(() => (typeof urlQuery?.search === 'string' ? urlQuery.search : ''), [urlQuery])
+  const query = normalizedUrlSearch && !_query ? normalizedUrlSearch : _query
+  const [showTooltip, setShowTooltip] = useState(false)
+  const handleChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value)
+  }
+  return (
+    <StyledCard>
+      <CurrentLaunchpadCardInnerContainer>
+        <Flex justifyContent="space-between" marginTop="5px">
+          <LaunchpadLogo>
+          <img src='https://photos.pinksale.finance/file/pinksale-logo-upload/1676403541600-81c759e1e8c327664374fc652ed436b1.png' alt='token logo'/>
+          </LaunchpadLogo>
+          
+          <Text color="green">Sale Live</Text>
+        </Flex>
+        <Text marginBottom={'10px'} color="textSubtle" fontSize="19px" textAlign="left">
+          Crazy Monkey
+        </Text>
+        <Text marginBottom={'0px'} color="textSubtle" fontSize="21px" textAlign="left">
+          Fair Launch - Max buy 0.35 BU...
+        </Text>
+        <Text marginBottom={'10px'} color="textSubtle" fontSize="17px" textAlign="left">
+          Soft
+        </Text>
+        <Text marginBottom={'10px'} color="textSubtle" fontSize="15px" textAlign="left">
+          5 BUSD
+        </Text>
+        <Text marginBottom={'2px'} color="textSubtle" fontSize="15px" textAlign="left">
+          Progress (107.87%)
+        </Text>
+        <progress value={70} max="100" style={{}} className="ProgressBar">
+          <ProgresBar />
+        </progress>
+        <Flex justifyContent="space-between" marginTop="10px">
+          <Text>Liquidity:</Text>
+          <Text>67%</Text>
+        </Flex>
+        <Flex justifyContent="space-between" marginTop="10px">
+          <Text>Lokup Time:</Text>
+          <Text>66666666 minutes</Text>
+        </Flex>
+        <Flex justifyContent="space-between" marginTop="10px">
+          <Text>Sales End in:</Text>
+          <Flex>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="37"
+              height="37"
+              fill="currentColor"
+              className="bi bi-heart-fill"
+              viewBox="0 0 16 16"
+              style={{ marginRight: '15px' }}
+            >
+              <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+            </svg>
+
+            <Link href={'/detail'}>
+              <button type="button" className="CurrentLaunchpadButton">
+                View
+              </button>
+            </Link>
+          </Flex>
+        </Flex>
+      </CurrentLaunchpadCardInnerContainer>
+    </StyledCard>
+  )
+}
+
+export default CurrentLaunchpadCard
