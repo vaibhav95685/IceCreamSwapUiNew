793d2738e4f8a3626a781334582b761b8795c941
Ritik <ritiktyagi01356@gmail.com>
2023-02-15 11:42:00 +0530

    commit

---
+++ apps/web/src/pages/launchpad-detail.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LaunchpadDetail from 'views/Launchpad/CurrentLaunchpad/LaunchpadDetaill'
+
+export default function launchpaddetail() {
+  return (
+    <>
+    <LaunchpadDetail/>
+    </>
+  )
+}
